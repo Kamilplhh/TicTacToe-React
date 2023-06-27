67e4e43508b886a85b56dbe1c75e128b65dde0ec
Kamilplhh <Kamil_PLHH@wp.pl>
2023-06-27 23:39:02 +0200

    Next move/winner status

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i) {
-    if (squares[i]) {
+    if (squares[i] || calculateWinner(squares)) {
       return;
     }
     const nextSquares = squares.slice();
@@ -27,25 +27,54 @@ export default function Board() {
     setXIsNext(!xIsNext);
   }
 
-  return (
-    <>
-      <div className="board">
-        <div className="row">
-          <Square value={squares[0]} SquareClick={() => handleClick(0)} />
-          <Square value={squares[1]} SquareClick={() => handleClick(1)} />
-          <Square value={squares[2]} SquareClick={() => handleClick(2)} />
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
+    const winner = calculateWinner(squares);
+    let status;
+    if (winner) {
+      status = "Winner: " + winner;
+    } else {
+      status = "Next player: " + (xIsNext ? "X" : "O");
+    }
+
+    return (
+      <>
+        <div className="status">{status}</div>
+        <div className="board">
+          <div className="row">
+            <Square value={squares[0]} SquareClick={() => handleClick(0)} />
+            <Square value={squares[1]} SquareClick={() => handleClick(1)} />
+            <Square value={squares[2]} SquareClick={() => handleClick(2)} />
+          </div>
+          <div className="row">
+            <Square value={squares[3]} SquareClick={() => handleClick(3)} />
+            <Square value={squares[4]} SquareClick={() => handleClick(4)} />
+            <Square value={squares[5]} SquareClick={() => handleClick(5)} />
+          </div>
+          <div className="row">
+            <Square value={squares[6]} SquareClick={() => handleClick(6)} />
+            <Square value={squares[7]} SquareClick={() => handleClick(7)} />
+            <Square value={squares[8]} SquareClick={() => handleClick(8)} />
+          </div>
         </div>
-        <div className="row">
-          <Square value={squares[3]} SquareClick={() => handleClick(3)} />
-          <Square value={squares[4]} SquareClick={() => handleClick(4)} />
-          <Square value={squares[5]} SquareClick={() => handleClick(5)} />
-        </div>
-        <div className="row">
-          <Square value={squares[6]} SquareClick={() => handleClick(6)} />
-          <Square value={squares[7]} SquareClick={() => handleClick(7)} />
-          <Square value={squares[8]} SquareClick={() => handleClick(8)} />
-        </div>
-      </div>
-    </>
-  );
-}
+      </>
+    );
+  }
