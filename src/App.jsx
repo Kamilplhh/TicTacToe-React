fb03951d4ea1a198836e75262fc08ce5429cc100
Kamilplhh <Kamil_PLHH@wp.pl>
2023-06-28 18:08:26 +0200

    Bug fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { useState } from 'react';
 import './index.css'
 
-function Square({ value, SquareClick }) {
+function Square({ value, onSquareClick }) {
   return (
-    <button className="square" onClick={SquareClick}>
+    <button className="square" onClick={onSquareClick}>
       {value}
     </button>
   );
@@ -60,19 +60,19 @@ export default function Board() {
         <div className="status">{status}</div>
         <div className="board">
           <div className="row">
-            <Square value={squares[0]} SquareClick={() => handleClick(0)} />
-            <Square value={squares[1]} SquareClick={() => handleClick(1)} />
-            <Square value={squares[2]} SquareClick={() => handleClick(2)} />
+            <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+            <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+            <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
           </div>
           <div className="row">
-            <Square value={squares[3]} SquareClick={() => handleClick(3)} />
-            <Square value={squares[4]} SquareClick={() => handleClick(4)} />
-            <Square value={squares[5]} SquareClick={() => handleClick(5)} />
+            <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+            <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+            <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
           </div>
           <div className="row">
-            <Square value={squares[6]} SquareClick={() => handleClick(6)} />
-            <Square value={squares[7]} SquareClick={() => handleClick(7)} />
-            <Square value={squares[8]} SquareClick={() => handleClick(8)} />
+            <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+            <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+            <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
           </div>
         </div>
       </>
